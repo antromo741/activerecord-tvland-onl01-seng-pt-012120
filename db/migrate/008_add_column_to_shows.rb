9c40cb9926e17b34fc1980b644e1847035740876
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-04-08 19:33:52 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_column_to_shows.rb
+++ db/migrate/008_add_column_to_shows.rb
@@ -1,5 +1,7 @@
 class AddColumnToShows < ActiveRecord::Migration[5.2]
-  def changeadd_column :shows, :day, :string
+  def change
+    add_column :shows, :day, :string
     add_column :shows, :genre, :string
-    
     add_column :shows, :season, :string
+  end
+end
